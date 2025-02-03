cb63e767b2efbad3229c0a2d297e201a89de26fe
Lucas Moreira <ms.lucasmoreira@gmail.com>
2025-02-03 20:30:27 -0300

    add import aliases configuration in api-rest-solid project

--- api-rest-solid/src/server.ts
+++ api-rest-solid/src/server.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 
 app
   .listen({
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: env.PORT,
   })
-  .then(() => console.log("HTTP server running..."));
+  .then(() => console.log('HTTP server running...'));
